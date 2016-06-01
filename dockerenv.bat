e20345427322b2f9f3183b91680a06be5d4a6459
sivalingapandian@yahoo.com <pandi12>
2016-06-01 09:40:09 -0500

    Added docker support

---
+++ dockerenv.bat
@@ -0,0 +1,4 @@
+set DOCKER_TLS_VERIFY=1
+set DOCKER_HOST=tcp://192.168.99.100:2376
+set DOCKER_CERT_PATH=C:\Users\Pandian.Velayutham\.docker\machine\machines\default
+set DOCKER_MACHINE_NAME=default
